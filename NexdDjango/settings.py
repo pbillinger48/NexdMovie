45354929bdbfb3485cc3b0c92a15ca7d264c784d
Parker <pbillinger48@gmail.com>
2023-02-13 19:10:37 -0600

    Added MovieApp Files and removed incorrect Model

--- NexdDjango/settings.py
+++ NexdDjango/settings.py
@@ -38,9 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'MovieApp.apps.MovieappConfig'
+
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheadrs.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
