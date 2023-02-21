444868ff3b153bdcdaa3e4a0105b9f8b7d29a61b
Parker <pbillinger48@gmail.com>
2023-02-21 14:35:03 -0600

    Added text input function

--- nexdreact/src/App.js
+++ nexdreact/src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import{ useState } from 'react';
 function App() {
+  const[username, setUsername] = useState('');
+  const [displayUsername, setDisplayUsername] = useState('');
+
+  const handleInputChange = (event) => {
+    setUsername(event.target.value);
+  };
+  const handleButtonClick = () => {
+    setDisplayUsername(username);
+  }
   return (
     <div className="App">
       <header className="App-Title">
@@ -13,14 +22,14 @@ function App() {
         <p>
           Insert Letterboxd Username: &nbsp;
         </p>
-        <input type= "text"/>
+        <input type= "text" value={username} onChange={handleInputChange}/>
         
-        <button>
+        <button onClick={handleButtonClick}>
         Enter Username
         </button>
       </header>
       <body className="App-Body">
-        <p>test</p>
+        <p>{displayUsername}</p>
       </body>
     </div>
   );
