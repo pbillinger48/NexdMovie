5f61a74a98fefb85687e6c092c1f9fd7834c7d28
Parker <pbillinger48@gmail.com>
2023-04-17 23:58:25 -0500

    Some basic UI work

--- nexdreact/src/HomePage.js
+++ nexdreact/src/HomePage.js
@@ -6,9 +6,10 @@ export default class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: '',
-      noviePosters: ['https://image.tmdb.org/t/p/w300/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg', 'https://image.tmdb.org/t/p/w300/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg', 'https://image.tmdb.org/t/p/w300/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg', 'https://image.tmdb.org/t/p/w300/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg','https://image.tmdb.org/t/p/w300/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg'], // an array of empty image sources      
-      moviePosters:['','','','','']
+      userName: '',    
+      noviePosters:['','','','',''],
+      movieTitles: [],
+      moviePosters: ['https://image.tmdb.org/t/p/w300/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg', 'https://image.tmdb.org/t/p/w300/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg', 'https://image.tmdb.org/t/p/w300/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg', 'https://image.tmdb.org/t/p/w300/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg','https://image.tmdb.org/t/p/w300/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg'], // an array of empty image sources  
     };
     this.handleButtonClick = this.handleButtonClick.bind(this);
   }
@@ -21,6 +22,12 @@ export default class HomePage extends Component {
   }
 
   handleButtonClick(){
+
+    this.setState({
+      moviePosters: ['','','','',''],
+      movieTitles: [],
+    });
+
     const requestOptions = {
         method: 'POST',
         headers:{'Content-Type':'application/json'},
@@ -32,11 +39,13 @@ export default class HomePage extends Component {
     response.json()
     ).then((data)=> {
       const moviePosters = [];
+      const movieTitles = [];
       for (const movie in data.movie_reccs_dict){
         const posterURL = 'https://image.tmdb.org/t/p/w300' + data.movie_reccs_dict[movie];
         moviePosters.push(posterURL);
+        movieTitles.push(movie);
       }
-      this.setState({ moviePosters });
+      this.setState({ moviePosters, movieTitles });
       console.log(data)
     });
   }
@@ -59,7 +68,10 @@ export default class HomePage extends Component {
         <body className="App-Body">
            {/* render the empty image sources */}
            {this.state.moviePosters.map((poster, index) => (
-            <img key={index} src={poster} alt={`movie poster ${index}`} style={{margin: "20px"}}/>
+            <div key={index} style={{ margin: '20px' }}>
+            <img src={poster} alt={`movie poster ${index}`} style={{width:'30vw', height:'45vh', maxWidth: '95%', overflow: 'hidden'}} />
+            <p>{this.state.movieTitles[index]}</p>
+            </div>
           ))}
         </body>
       </div>
